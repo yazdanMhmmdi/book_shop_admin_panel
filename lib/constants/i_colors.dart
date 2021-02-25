f7f614f3860ccfaefefcd8cd40919d99e4312cde
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-02-25 14:17:01 +0330

    add some colors

---
+++ lib/constants/i_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class IColors {
+  static final Color green = Color(0xffA2DE96);
+  static final Color boldGreen = Color(0xff3CA59E);
+  static final Color lowBoldGreen = Color(0xff3CA59E).withOpacity(0.15);
+  static final Color black85 = Color(0xff000000).withOpacity(0.85);
+  static final Color black35 = Color(0xff000000).withOpacity(0.35);
+  static final Color black25 = Color(0xff000000).withOpacity(0.25);
+  static final Color white90 = Color(0xffFFFFFF).withOpacity(0.90);
+}
