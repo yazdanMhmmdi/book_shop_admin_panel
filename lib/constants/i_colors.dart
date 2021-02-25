ea33ebbed44d4bb62890d90eca22af44a2d73c06
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-02-25 14:28:15 +0330

    implement action bar, side bar and main panel

--- lib/constants/i_colors.dart
+++ lib/constants/i_colors.dart
@@ -3,7 +3,8 @@ import 'package:flutter/cupertino.dart';
 class IColors {
   static final Color green = Color(0xffA2DE96);
   static final Color boldGreen = Color(0xff3CA59E);
-  static final Color lowBoldGreen = Color(0xff3CA59E).withOpacity(0.15);
+  static final Color boldGreen15 = Color(0xff3CA59E).withOpacity(0.15);
+  static final Color lowBoldGreen = Color(0xffE2F1F0);
   static final Color black85 = Color(0xff000000).withOpacity(0.85);
   static final Color black35 = Color(0xff000000).withOpacity(0.35);
   static final Color black25 = Color(0xff000000).withOpacity(0.25);
--- lib/presentation/screen/panel_screen.dart
+++ lib/presentation/screen/panel_screen.dart
@@ -1,11 +1,35 @@
+import 'package:book_shop_admin_panel/constants/i_colors.dart';
 import 'package:flutter/material.dart';
-
 
 class PanelScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Simple Text'),
+      backgroundColor: IColors.lowBoldGreen,
+      body: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Column(
+          children: [
+            Container(height: 64, color: Colors.blue),
+            Row(
+              children: [
+                Container(
+                  width: 77,
+                  height: MediaQuery.of(context).size.height - 64,
+                  color: Colors.yellow,
+                  child: Text('a'),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 77,
+                  height: MediaQuery.of(context).size.height - 64,
+                  color: Colors.red,
+                  child: Text("b"),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
