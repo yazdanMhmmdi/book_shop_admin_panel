a063dcad72d980dd57f44f2456e27970f7afab93
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-02-26 12:24:10 +0330

    implement circular double tap on category items

--- lib/presentation/widget/category_item.dart
+++ lib/presentation/widget/category_item.dart
@@ -1,5 +1,7 @@
 import 'package:book_shop_admin_panel/constants/assets.dart';
 import 'package:book_shop_admin_panel/constants/i_colors.dart';
+import 'package:book_shop_admin_panel/presentation/widget/circular_double_tap.dart';
+import 'package:book_shop_admin_panel/presentation/widget/circular_on_tap.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
@@ -17,20 +19,25 @@ class CategoryItem extends StatelessWidget {
           borderRadius: BorderRadius.circular(18),
           color: IColors.lowBoldGreen,
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            child,
-            SizedBox(height: 5),
-            Text(
-              title,
-              style: TextStyle(
-                color: IColors.black85,
-                fontFamily: "IranSans",
-                fontSize: 16,
+        child: CircularDoubleTap(
+          onDoubleTap: () {
+            print('Test2');
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              child,
+              SizedBox(height: 5),
+              Text(
+                title,
+                style: TextStyle(
+                  color: IColors.black85,
+                  fontFamily: "IranSans",
+                  fontSize: 16,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
