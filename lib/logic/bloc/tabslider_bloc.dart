fdbb9c8240354d84af5650fa57450d0639dd5959
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-04-06 01:42:09 +0430

    prepare network & api provider

--- lib/logic/bloc/tabslider_bloc.dart
+++ lib/logic/bloc/tabslider_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:book_shop_admin_panel/presentation/tab/books_tab.dart';
+import 'package:book_shop_admin_panel/presentation/tab/category_tab.dart';
 import 'package:book_shop_admin_panel/presentation/tab/users_tab.dart';
 import 'package:equatable/equatable.dart';
 
@@ -17,7 +18,7 @@ class TabsliderBloc extends Bloc<TabsliderEvent, TabsliderState> {
   ) async* {
     if (event is MoveForwardEvent) {
       if (event.tab == 0)
-        yield TabsliderSuccess(BooksTab());
+        yield TabsliderSuccess(CategoryTab());
       else
         yield TabsliderSuccess(UsersTab());
     }
---
+++ lib/networking/api_provider.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class ApiProvider {
+  static const String URL_IP = "localhost";//yazdanmohammadi.ir
+  final String _BASE_URL = "https://$URL_IP/book_shop/api/admin/";
+
+  Future<dynamic> get(String url) async {
+    try {
+      final response = await http.get(_BASE_URL + url);
+      return await decodeResponse(response);
+    } catch (_) {
+      print('connection failure $_BASE_URL' + url);
+    }
+  }
+
+  dynamic decodeResponse(response) {
+    try {
+      return json.decode(response.body);
+    } catch (_) {
+      print("json decoding failure");
+    }
+  }
+  // decodeResponse(bool utf8Support, response) {
+  //   if (utf8Support) {
+  //     return json.decode(utf8.decode(response.bodyBytes));
+  //   } else {
+  //     return json.decode(response.body.toString());
+  //   }
+  // }
+}
